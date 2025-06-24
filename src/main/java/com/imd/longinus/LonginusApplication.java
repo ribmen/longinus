7f96f128ea2d3f00616919122b523e0f1f7bb35d
Beatriz Pegado <anabeatrizdasilvapegado@gmail.com>
2025-06-24 13:35:42 -0300

    ajeita application e modifica application.properties

--- src/main/java/com/imd/longinus/LonginusApplication.java
+++ src/main/java/com/imd/longinus/LonginusApplication.java
@@ -3,7 +3,9 @@ package com.imd.longinus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration.class
+	})
 public class LonginusApplication {
 
 	public static void main(String[] args) {
